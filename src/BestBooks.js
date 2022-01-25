600c1ae608ea33b9e197d49da1fd7407cd60fb72
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-01-24 19:08:49 -0800

    added frontend components of lab 11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,28 +1,71 @@
 import React from 'react';
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      //renderBooks: false
+
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch books for the logged in user  */
+
+  getBooksInfo = async () => {
+    console.log('error');
+    try{let bookData = await axios.get('http://localhost:3001/books')
+    //console.log(bookData.data);
+
+    this.setState({
+      books: bookData.data
+    })}catch(e){console.log(e.message)};
+  }
+
+
+  componentDidMount () {
+    this.getBooksInfo();
+    //console.log('books');
+  }
+
+
+
 
   render() {
+    console.log(this.state.books);
+    
+    let booksToRender = this.state.books.map((book, idx) => 
+      <Carousel.Item key={idx}>
+        <img src= "https://via.placeholder.com/400x550/333/808080%20C/O%20https://placeholder.com/" alt='hello'></img>
 
-    /* TODO: render user's books in a Carousel */
+        <Carousel.Caption>
+          <h3>{book.title}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      
+    )
+
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0 ? (
+          <Carousel>
+          {booksToRender}
+        </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
+
+        
+
+
+
+
+
+
       </>
     )
   }
