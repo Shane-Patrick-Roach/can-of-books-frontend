72026432b48ce1124dadccbfcd138464524e2139
joshuamccluskey <jpiff57@gmail.com>
2022-01-29 17:51:21 -0800

    OAuth complete Profile pulling user information

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,31 +1,37 @@
-import { Component } from 'react'
-import LoginForm from './LoginForm'
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import Button from "react-bootstrap/Button";
 
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
 
-
-class LoginButton extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      showButton: true
-    }
-  }
-
-  render() {
-
-
-    /* TODO: Render a button with label 'Log In'. When the button is clicked then show LoginForm instead */
-    return (
-      <>
-      {this.state.showButton ? <Button onClick={() => this.setState({ showButton: false})}>Log In</Button> : 
-
-      <LoginForm onLogin={this.props.onLogin}/>}
-      </>
-    
-    )
-  }
-}
+  return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+};
 
 export default LoginButton;
+
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     showButton: true
+  //   }
+  // }
+
+//   render() {
+
+
+//     /* TODO: Render a button with label 'Log In'. When the button is clicked then show LoginForm instead */
+//     return (
+//       <>
+//       {this.state.showButton ? <Button onClick={() => this.setState({ showButton: false})}>Log In</Button> : 
+
+//       <LoginForm onLogin={this.props.onLogin}/>}
+//       </>
+    
+//     )
+//   }
+// }
+
+// const loginButton = ()
+
+// export default LoginButton;
--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,24 @@
 import { Component } from "react";
+import { withAuth0 } from '@auth0/auth0-react';
 
 class Profile extends Component {
 
-  render() {
-    return <p>{this.props.user.username} with an email of {this.props.user.email}</p>
+
+
+  
+
+  render()
+  {
+    console.log(this.props.auth0.user.given_name)
+    return(
+      <p>{this.props.auth0.user.given_name} with an email of {this.props.auth0.user.email}</p>
+    
+
+    );
+
+
+
   }
 };
 
-export default Profile;
+export default withAuth0(Profile);
